406378324ec2ec0adedc09f030b4a0895907c4f1
akakou <s16003@tokyo.kosen-ac.jp>
2018-08-15 01:45:35 +0900

    [add] MSTest Project
     Add the MSTest project

---
+++ Filt.Tests/ClientTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Filt.Tests
+{
+    [TestClass]
+    public class ClientTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Filt.Tests/ScannerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Filt.Tests
+{
+    [TestClass]
+    public class ScannerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
